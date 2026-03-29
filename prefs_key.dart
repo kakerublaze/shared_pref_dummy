f2fd2c274bf21e894946ff5e6cbc990dfde24b1d
Taher Dodia <kakerublaze@gmail.com>
2026-03-29 23:46:32 +0530

    Create prefs_key.dart

---
+++ prefs_key.dart
@@ -0,0 +1,7 @@
+class PrefKeys {
+  static const isLoggedIn = "isLoggedIn";
+  static const email = "email";
+  static const token = "token";
+  static const user = "user";
+  static const productList = "productList";
+}
